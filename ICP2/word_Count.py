2f6fa7c19f603fb13bd00429f045a2c4b9dcfb7a
sameer999 <ysskc99@gmail.com>
2019-02-01 19:01:08 -0600

    icp2

--- ICP2/word_Count.py
+++ ICP2/word_Count.py
@@ -1,7 +1,11 @@
 with open('abc.txt') as f:
   lines = f.readlines()
-  data = ''.join(lines)
-  print('lines =',len(lines))
-  print('Words = ',len(data.split()))
-  char = ''.join(data.split())
-  print('characters = ',len(char))
+  print(lines)
+  print('lines =', len(lines))
+  for i in lines:
+    data = ''.join(i)
+    #print(data)
+
+    print('Words = ',len(data.split()))
+    char = ''.join(data.split())
+    print('characters = ',len(char))
