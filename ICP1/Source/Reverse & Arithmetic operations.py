9aab58edeebe8acd0b75e334f2ad03213b2a7c1b
sameer999 <ysskc99@gmail.com>
2019-02-01 19:56:35 -0600

    Icp1

--- ICP1/Source/Reverse & Arithmetic operations.py
+++ ICP1/Source/Reverse & Arithmetic operations.py
@@ -1,13 +1,15 @@
 a=input("Enter first number:")
 b=input("Enter second number:")
+
+rev=a[::-1]
+print("reverse of first number is:",int(rev))
+
 a=int(a)
 b=int(b)
+print('arithmetic operations:')
 numbers=[a,b]
 c=sum(numbers)
 print("Sum is:",c)
-c=str(c)
-rev=c[::-1]
-print("reverse is:",rev)
 print("subtraction:",(a-b))
 print("multiplication:",(a*b))
 print("division:",(a/b))
