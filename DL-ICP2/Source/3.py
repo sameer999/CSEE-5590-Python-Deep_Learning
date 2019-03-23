d936d3452f384de82210b4e79e8b314417017b6a
sameer999 <ysskc99@gmail.com>
2019-03-22 19:46:01 -0500

    DL icp2

---
+++ DL-ICP2/Source/3.py
@@ -0,0 +1,66 @@
+from keras.models import Sequential
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+(train_images,train_labels),(test_images, test_labels) = mnist.load_data()
+#display the first image in the training data
+plt.imshow(train_images[0,:,:],cmap='gray')
+plt.title('Ground Truth : {}'.format(train_labels[0]))
+plt.show()
+
+#process the data
+#1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
+dimData = np.prod(train_images.shape[1:])
+train_data = train_images.reshape(train_images.shape[0],dimData)
+test_data = test_images.reshape(test_images.shape[0],dimData)
+
+#convert data to float and scale values between 0 and 1
+train_data = train_data.astype('float')
+test_data = test_data.astype('float')
+#scale data
+train_data /=255.0
+test_data /=255.0
+#change the labels frominteger to one-hot encoding
+train_labels_one_hot = to_categorical(train_labels)
+test_labels_one_hot = to_categorical(test_labels)
+
+#creating network
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(dimData,)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(252, activation='tanh'))
+model.add(Dense(400, activation='sigmoid'))
+model.add(Dense(10, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_data, train_labels_one_hot, batch_size=256, epochs=6, verbose=1,
+                   validation_data=(test_data, test_labels_one_hot))
+
+[test_loss, test_acc] = model.evaluate(test_data, test_labels_one_hot)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+'''image_index = 130
+plt.imshow(test_data[image_index].reshape(28, 28),cmap='Greys')
+plt.title("Digit from the test data")
+plt.show()
+pred = model.predict(test_data[image_index].reshape(1,784))
+print("Predicted digit:",pred.argmax())'''
+#print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
